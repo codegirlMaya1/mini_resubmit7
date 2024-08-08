f7296af3bc3d604ec179582fd1e768b4276ed79f
codegirlMaya1 <ebonyslove209@gmail.com>
2024-08-08 06:04:24 -0600

    Add files via upload

---
+++ dumps/Dump20240807 (1)/gym_db_workoutsessions.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gym_db
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `workoutsessions`
+--
+
+DROP TABLE IF EXISTS `workoutsessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `workoutsessions` (
+  `session_id` int NOT NULL,
+  `member_id` int DEFAULT NULL,
+  `session_date` date DEFAULT NULL,
+  `session_time` varchar(50) DEFAULT NULL,
+  `activity` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `workoutsessions_ibfk_1` FOREIGN KEY (`member_id`) REFERENCES `members` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `workoutsessions`
+--
+
+LOCK TABLES `workoutsessions` WRITE;
+/*!40000 ALTER TABLE `workoutsessions` DISABLE KEYS */;
+INSERT INTO `workoutsessions` VALUES (1,1,'2023-06-15','06:00:00','5'),(2,2,'2023-07-15','06:30:00','6'),(3,3,'2023-08-15','10:30:00','7'),(4,4,'2023-09-15','10:30:00','8');
+/*!40000 ALTER TABLE `workoutsessions` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-08-08  5:58:08
